ad7edfac660c735f37e88d973a85bafefd232778
irimpangeorge <georgejirimpan@gmail.com>
2025-05-06 18:22:55 +0530

    Update app.py
    added rollover option
    fixed bugs
    added option to show current orders

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import json
 import os
 from datetime import datetime, timedelta
 
-
 # ---------------------- CONFIG ----------------------
 #API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzQ3OTAxNjU5LCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDEwODg5MyJ9.Bg1TsNnNTRd6znWPQNgcBB4OAW8I0zjQmwjDcs-o2k3dJJlvGDPnmVgYFb82ID1sur6wN7lNtSh-tnH1L6dGyg"  # Replace with your Dhan access token
-API_URL = "https://api.dhan.co/v2/charts/intraday"
+API_HISTORY_URL = "https://api.dhan.co/v2/charts/intraday"
+API_CURR_POSITIONS = "https://api.dhan.co/v2/positions"
 
 
 USERNAME = "admin"
@@ -29,10 +29,10 @@ if "logged_in" not in st.session_state:
 
 # List of instruments with mock securityIds (replace with actuals)
 instruments = {
-    "NSE_NIFTY-I": {"Id": "1", "segment": "NSE_FNO","instrumentID":"FUTIDX", "timeframe": 75, "expiry": "2025-04-24"},
-    "NSE_NIFTYBANK-I": {"Id": "2", "segment": "NSE_FNO","instrumentID":"FUTIDX", "timeframe": 75, "expiry": "2025-04-24"},
-    "MCX_GOLDM-I": {"Id": "3", "segment": "MCX_COMM","instrumentID":"FUTCOM", "timeframe": 60, "expiry": "2025-05-05"},
-    "MCX_SILVERM-I": {"Id": "4", "segment": "MCX_COMM","instrumentID":"FUTCOM", "timeframe": 60, "expiry": "2025-04-30"},
+    "NSE_NIFTY-I": {"Id": "1", "securityID": "0", "segment": "NSE_FNO","instrumentID":"FUTIDX", "timeframe": 75, "expiry": "2025-04-24"},
+    "NSE_NIFTYBANK-I": {"Id": "2", "securityID": "0", "segment": "NSE_FNO","instrumentID":"FUTIDX", "timeframe": 75, "expiry": "2025-04-24"},
+    "MCX_GOLDM-I": {"Id": "3", "securityID": "0", "segment": "MCX_COMM","instrumentID":"FUTCOM", "timeframe": 60, "expiry": "2025-05-05"},
+    "MCX_SILVERM-I": {"Id": "4", "securityID": "0", "segment": "MCX_COMM","instrumentID":"FUTCOM", "timeframe": 60, "expiry": "2025-04-30"},
 }
 
 # ---------------------- FUNCTIONS ----------------------
@@ -42,7 +42,7 @@ def login():
     with password_holder.container():
         st.title("🔐 Login")
         username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
+        password = st.text_input("Password", type="password") 
         if st.button("Login"):
             if username == USERNAME and password == PASSWORD:
                 st.session_state["logged_in"] = True
@@ -55,27 +55,28 @@ def login():
                 st.error("Invalid username or password")
 
 def fetch_and_displaydata(instrument, atrperiod, multiplier, timeframe, quantity, dhan_api):
-    if is_mcx_tender_period(instrument):
-        st.warning("🚫 MCX trade skipped due to tender period (3 days before expiry).")
-    else:
-        df = fetch_data(instrument, timeframe, dhan_api)
 
-        if not df.empty:
-            df = apply_indicators(df, atrperiod, multiplier )
-            df = generate_signals(df)
+    securityid, expirydate = get_security_id(instrument)
 
-            st.subheader("📊 Signals Table")
-            st.dataframe(df[["close", "supertrend", "di_plus", "di_minus", "entry", "exit"]].tail(2000))
+    df = fetch_data(instrument, timeframe, dhan_api, securityid)
 
-            #st.subheader("📉 Price vs Supertrend")
-            #st.line_chart(df[["close", "supertrend"]])
+    if not df.empty:
+        df = apply_indicators(df, atrperiod, multiplier )
+        df = generate_signals(df)
 
-            st.subheader("📍 Last Signal")
-            latest_signal = df["entry"].dropna().iloc[-1] if not df["entry"].dropna().empty else "No signal"
-            st.success(f"🔔 Last Signal: {latest_signal}")
+        if is_mcx_tender_period(instrument, expirydate):
+            st.warning("🚫 MCX trade skipped due to tender period (5 days before expiry).\nPlease use roll over option to place next order")
+
+        st.subheader("📊 Signals Table")
+        st.dataframe(df[["close", "supertrend", "di_plus", "di_minus", "entry", "exit"]].tail(2000))
+
+        st.subheader("📍 Last Signal")
+
+        latest_signal = df["entry"].dropna().iloc[-1] if not df["entry"].dropna().empty else "No signal"
+        st.success(f"🔔 Last Signal: {latest_signal}")
         return
 
-def get_security_id(instrument):
+def get_security_id(instrument, count=0):
     ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 # Step 2: Download the file manually
@@ -115,19 +116,70 @@ def get_security_id(instrument):
     securityid = securityid.sort_values("SM_EXPIRY_DATE")
 
 # Get the latest expiry
-    latest_securityid = securityid["SECURITY_ID"].iloc[0]
-    latest_expirydate = securityid["SM_EXPIRY_DATE"].iloc[0]
+    latest_securityid = securityid["SECURITY_ID"].iloc[count]
+    latest_expirydate = securityid["SM_EXPIRY_DATE"].iloc[count]
     
     return latest_securityid, latest_expirydate
 
-def is_mcx_tender_period(instrument):
-    securityid, expirydate = get_security_id(instrument)
+def get_next_order(instrument, count=0):
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+# Step 2: Download the file manually
+    url = "https://images.dhan.co/api-data/api-scrip-master-detailed.csv"
+    context = ssl.create_default_context(cafile=certifi.where())
+    with urllib.request.urlopen(url, context=context) as response:
+        csv_data = response.read().decode("utf-8")
+
+# Step 3: Load into DataFrame
+    scrip_master = pd.read_csv(StringIO(csv_data))
+    if(instrument["Id"] == "1"):
+        securityid = scrip_master[
+            (scrip_master["INSTRUMENT"] == "FUTIDX") &
+            (scrip_master["UNDERLYING_SYMBOL"]=="NIFTY") &
+            (scrip_master["EXCH_ID"] == "NSE")
+            ].copy()
+    elif(instrument["Id"] == "2"): 
+        securityid = scrip_master[
+            (scrip_master["INSTRUMENT"] == "FUTIDX") &
+            (scrip_master["UNDERLYING_SYMBOL"]=="BANKNIFTY") &
+            (scrip_master["EXCH_ID"] == "NSE")
+            ].copy()   
+    elif(instrument["Id"] == "3"): 
+        securityid = scrip_master[
+            (scrip_master["INSTRUMENT"] == "FUTCOM") &
+            (scrip_master["UNDERLYING_SYMBOL"]=="GOLDM") &
+            (scrip_master["EXCH_ID"] == "MCX")
+            ].copy()   
+    else:
+        securityid = scrip_master[
+            (scrip_master["INSTRUMENT"] == "FUTCOM") &
+            (scrip_master["UNDERLYING_SYMBOL"]=="SILVERM") &
+            (scrip_master["EXCH_ID"] == "MCX")
+            ].copy()   
+
+    securityid["SM_EXPIRY_DATE"] = pd.to_datetime(securityid["SM_EXPIRY_DATE"], errors="coerce")
+    securityid = securityid.sort_values("SM_EXPIRY_DATE")
+
+# Get the latest expiry
+    next_order = securityid["SYMBOL_NAME"].iloc[count]
+
+    return next_order
+
+
+def is_mcx_tender_period(instrument, expirydate):
     ts = pd.Timestamp(expirydate)
     expiry = ts.date() #datetime.strptime(expirydate, "%Y-%m-%d")
-    tender_start = expiry - timedelta(days=3)
+    tender_start = expiry - timedelta(days=6)
     return datetime.now().date() >= tender_start and instrument["segment"] == "MCX_COMM"
 
-def fetch_data(instrument, timeframe, dhan_api):
+def is_option_tender_period_expired(instrument, expirydate):
+    ts = pd.Timestamp(expirydate)
+    expiry = ts.date() #datetime.strptime(expirydate, "%Y-%m-%d")
+    tender_start = expiry - timedelta(days=1)
+    return datetime.now().date() >= tender_start and instrument["segment"] == "NSE_FNO"
+
+
+def fetch_data(instrument, timeframe, dhan_api, securityid):
     today = datetime.today()
     from_date = (today - timedelta(days=90)).strftime("%Y-%m-%d")
     to_date = today.strftime("%Y-%m-%d")
@@ -136,7 +188,6 @@ def fetch_data(instrument, timeframe, dhan_api):
         st.warning("Enter a valid api token and try again")
         return pd.DataFrame()
         
-    securityid, expirydate = get_security_id(instrument)
     payload = {
             "securityId": str(securityid),
             "exchangeSegment": instrument["segment"],
@@ -153,7 +204,7 @@ def fetch_data(instrument, timeframe, dhan_api):
 
     }
 
-    res = requests.post(API_URL, json=payload, headers=headers)
+    res = requests.post(API_HISTORY_URL, json=payload, headers=headers)
     if res.status_code != 200:
         st.error(f"Failed to fetch data: {res.status_code} - {res.text}")
         return pd.DataFrame()
@@ -183,6 +234,50 @@ def fetch_data(instrument, timeframe, dhan_api):
         "volume": "sum"
     }).dropna()
 
+def prevent_rerun(trigger_key="run_logic"):
+    """Call this function to control rerun behavior."""
+    if trigger_key not in st.session_state:
+        st.session_state[trigger_key] = False
+
+
+def fetch_current_orders(dhan_api,instrument):
+    st.subheader("📦 Current Orders")
+    headers = {
+        "access-token": dhan_api,
+	"Accept": "application/json"
+    }
+
+    res = requests.get(API_CURR_POSITIONS, headers=headers)
+
+    if res.status_code != 200:
+        st.error(f"Failed to fetch data: {res.status_code} - {res.text}")
+        return pd.DataFrame()
+
+    data = res.json()
+    if not data:
+        st.warning("No data returned.")
+        return pd.DataFrame()
+    
+    df = pd.DataFrame(data, columns=["tradingSymbol", "positionType", "exchangeSegment", "productType"])
+    fut_df = df[df["tradingSymbol"].str.contains("FUT", na=False)]
+    fut_df = fut_df[fut_df["productType"].str.contains("MARGIN", na=False)]
+    if(instrument["Id"] == "1"):
+        fut_df = fut_df[fut_df["tradingSymbol"].str.contains("NIFTY", na=False)] & ~df["tradingSymbol"].str.contains("BANKNIFTY", na=False)
+    elif(instrument["Id"] == "2"): 
+        fut_df = fut_df[fut_df["tradingSymbol"].str.contains("BANKNIFTY", na=False)]
+    elif(instrument["Id"] == "3"): 
+        fut_df = fut_df[fut_df["tradingSymbol"].str.contains("GOLDM", na=False)]
+    else:
+        fut_df = fut_df[fut_df["tradingSymbol"].str.contains("SILVERM", na=False)]
+    st.dataframe(fut_df[["tradingSymbol", "positionType", "exchangeSegment", "productType"]].tail(2000))
+    latest_contract = fut_df["tradingSymbol"].dropna().iloc[-1] if not fut_df["tradingSymbol"].dropna().empty else "No Contract"
+    st.success(f"📝  Current contract: {latest_contract}")
+    if latest_contract != "No Contract":
+        next_contract =  get_next_order(instrument, 1)
+    else:
+        next_contract =  get_next_order(instrument, 0)
+    return fut_df, next_contract
+
 def apply_indicators(df, atr_period, multipliers):
     st_indicator = ta.supertrend(df["high"], df["low"], df["close"], length = atr_period, multiplier = multipliers)
     trend = f"SUPERT_{atr_period}_{float(multipliers)}"
@@ -204,6 +299,7 @@ def display_supertrend():
             st.session_state.logged_in = False
             st.rerun()
 
+
     st.title("📈 Futures Supertrend(10,2)-ADX")
 
     instrument_name = st.selectbox("Select Instrument", list(instruments.keys()))
@@ -212,17 +308,36 @@ def display_supertrend():
     instrument_settings = load_instrument_settings(instrument)
     common_settings = load_common_settings()
 
+
     st.sidebar.header("Parameters")
 
     dhan_api_token = st.sidebar.text_input("Api Token", value = common_settings.get("dhan_api_token",""))
+    dhan_client_id = st.sidebar.text_input("Client ID", value = common_settings.get("dhan_client_id",""))
     nf_atr_period = st.sidebar.number_input("ATR Period", min_value = 0, max_value = None, value = int(instrument_settings["atr_period"]), step = 1)
     nf_multiplier = st.sidebar.number_input("Multiplier", min_value = 0, max_value = None, value = int(instrument_settings["multiplier"]), step = 1)
     nf_timeframe = st.sidebar.number_input("Time Frame", min_value = 0, max_value = None, value = int(instrument_settings["time_frame"]), step = 1)
     nf_quantity = st.sidebar.number_input("Quantity", min_value = 0, max_value = None, value = int(instrument_settings["quantity"]), step = 1)
 
-    sidecol1, sidecol2 = st.sidebar.columns([1, 1]) 
-    with sidecol1:
-        if st.button("💾 Save Settings"):
+   
+    fetch_and_displaydata(instrument, nf_atr_period, nf_multiplier, nf_timeframe, nf_multiplier, dhan_api_token)
+    currorder, next_contract = fetch_current_orders(dhan_api_token, instrument)
+    col1, col2 = st.sidebar.columns([1, 1]) 
+
+    with col2:
+        if st.button("Roll Over"):
+            latest_contract = currorder["tradingSymbol"].dropna().iloc[-1] if not currorder["tradingSymbol"].dropna().empty else "No Contract"
+            if latest_contract != "No Contract":
+                st.write("Current contract is:" +latest_contract+ ".\nAre you sure you want to roll over to " + next_contract + "?")
+                answer = st.radio("", ["Yes", "No"], label_visibility="collapsed" )
+                if st.button("Submit"):
+                    st.write(f"You selected: {answer}")
+            else:
+                st.write("Currently no contract. Do you want to roll over to " + next_contract + "?")
+                answer = st.radio("", ["Yes", "No"], label_visibility="collapsed" )
+                if st.button("Submit"):
+                    st.write(f"You selected: {answer}")
+    with col1:
+        if st.button("💾 Save"):
             instrument_settings = {
                 "atr_period": nf_atr_period,
                 "multiplier": nf_multiplier,
@@ -230,17 +345,14 @@ def display_supertrend():
                 "quantity": nf_quantity
                 }
             common_settings = {
+                "dhan_client_id": dhan_client_id,
                 "dhan_api_token": dhan_api_token
             }
             save_settings(instrument_settings, common_settings, instrument)
             st.success("✅ Settings saved permanently!")
             st.rerun()  # Optional: to reflect updated values immediately
-    
-    with sidecol2:
-        st.button("Roll Over")
-    API_TOKEN = dhan_api_token
+
     st.query_params["auth"] = "1"
-    fetch_and_displaydata(instrument,nf_atr_period,nf_multiplier,nf_timeframe,nf_multiplier,dhan_api_token)
 
 def generate_signals(df):
     df["long_entry"] = (df["close"] > df["supertrend"]) & (df["di_plus"] > df["di_minus"])
